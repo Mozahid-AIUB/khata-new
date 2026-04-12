b81d089086caa3b339efab36cdf02bf54ef86f86
mozahid <mozahidislam343@gmail.com>
2026-04-13 00:53:47 +0600

    fix vercel django config

--- api/index.py
+++ api/index.py
@@ -1,11 +1,11 @@
 import os
 import sys
+import django
 
-# Add the project root to Python path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-from config.wsgi import application
+django.setup()
 
+from config.wsgi import application
 app = application
